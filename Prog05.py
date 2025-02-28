1f728add0771ae77b133ffc521b2f928bc00e6e0
AceFVA <acefrancisagustin@gmail.com>
2025-03-01 02:23:33 +0800

    Prog05.py

---
+++ Prog05.py
@@ -0,0 +1,6 @@
+# Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+    
+user_num1 = float(input("Enter your first number: "))
+user_num2 = float(input("Enter your second number: "))
+
+print(user_num1 / user_num2)
