a3cde89e2706c9f2bf9a212fc66e11a7c31c0863
AceFVA <acefrancisagustin@gmail.com>
2025-03-01 02:14:53 +0800

    Prog01.py

---
+++ Prog01.py
@@ -0,0 +1,10 @@
+# Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
+user_num1 = int(input("Enter your first number: "))
+user_num2 = int(input("Enter your second number: "))
+
+if user_num1 > user_num2:
+    print(user_num1)
+
+else:
+    print(user_num2)
