f2423eafe48a8c00a1a2da5eeb93476dae616303
AceFVA <acefrancisagustin@gmail.com>
2025-03-01 20:01:57 +0800

    Prog08.py

---
+++ Prog08.py
@@ -0,0 +1,10 @@
+num = input("Enter 10 numbers separated in spaces: ")
+
+num_list = num.split()
+total_odd = 0
+
+for i in range(0,10):
+    if int(num_list[i]) % 2 == 1:
+        total_odd = total_odd + 1
+        
+print(total_odd)
